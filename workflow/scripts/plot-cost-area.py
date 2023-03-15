9bea296c1c778bf2a3fd587b03e7a5e6424b1e72
Fabian <fab.hof@gmx.de>
2023-03-15 19:30:27 +0100

    eem: continue writing on paper

--- workflow/scripts/plot-cost-area.py
+++ workflow/scripts/plot-cost-area.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import warnings
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -29,36 +28,55 @@ for path in snakemake.input.networks:
     opex = n.statistics.opex(aggregate_time="sum")
 
     costs = capex.add(opex, fill_value=0)
-    costs = costs.droplevel(0)
+    costs = costs.droplevel(0)[lambda x: x > 0]
+    costs = costs.groupby(costs.index).sum()
 
     design, sequestration = Path(path).stem.split("_")
-    key = (snakemake.config["labels"][design], int(sequestration))
+    key = design, int(sequestration)
 
     df[key] = costs
 df = pd.concat(df, axis=1)
 
+groups = snakemake.config["plotting"]["technology_groups"]
+grouped = df.groupby(groups).sum()
+grouped = grouped[grouped.max(axis=1) > 5e4]
 
-# do some grouping
+colors = snakemake.config["plotting"]["technology_group_colors"]
 
-
-norm = 1e6
-unit = "M€"
-nice_name = n.carriers.nice_name
-colors = n.carriers.color.dropna().rename(nice_name)
+norm = 1e9
+unit = "bn€/a"
 sort_by_color = (
-    lambda df: df.assign(color=colors[df.index])
-    .sort_values(by="color")
-    .drop("color", axis=1)
+    lambda df: df.assign(color=colors).sort_values(by="color").drop("color", axis=1)
 )
+sort_by_diff = (
+    lambda df: df.assign(diff=df.max(axis=1) - df.min(axis=1))
+    .sort_values(by="diff")
+    .drop("diff", axis=1)
+)
+grouped = sort_by_diff(grouped).div(norm)
 
-fig, ax = plt.subplots(1, 1, figsize=(3, 3.5), layout="constrained")
-df.plot(kind="area", stacked=True, ax=ax, color=colors.to_dict(), alpha=0.8, lw=0)
+fig, axes = plt.subplots(1, 2, figsize=(7, 3.0), layout="constrained")
 
-ax.axhline(0, color="k", lw=1)
-ax.set_xlabel("Sequestration Potential [Mt]")
-ax.set_ylabel("System cost [{unit}]")
-ax.grid(axis="y", alpha=0.5)
+for key, ax in zip(snakemake.config["scenarios"]["design"], axes):
+
+    grouped[key].T.plot(kind="area", stacked=True, ax=ax, color=colors, alpha=0.8, lw=0)
+
+    ax.axhline(0, color="k", lw=1)
+    ax.set_xlabel("Sequestration Potential [Mt]")
+    ax.set_ylabel(f"System cost [{unit}]")
+    ax.grid(axis="y", alpha=0.5)
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend().remove()
+    ax.set_title(snakemake.config["labels"][key].title().replace("Co", "CO"))
+
 
 sns.despine()
-
+fig.legend(
+    handles[::-1],
+    labels[::-1],
+    loc="center left",
+    bbox_to_anchor=(1, 0.5),
+    frameon=False,
+    ncol=1,
+)
 fig.savefig(snakemake.output[0], bbox_inches="tight", dpi=300)
